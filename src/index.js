eae9f1b4cf997c72195d01212a33b6a4d34033c2
Eduardo Pinto <edugrp@gmail.com>
2021-12-09 17:34:42 -0500

    Teste da API e exibindo as primeiras imagens

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const restEndpoint = "https://api.pexels.com/v1/search?query=nature&per_page=10&locale=pt-BR";
+
+const callRestApi = async () => {
+    const response = await fetch(restEndpoint, {
+        method: 'get',
+        headers: new Headers({
+            'Authorization': 'Bearer 563492ad6f91700001000001dac507b22f25415c861a3bd1f27322ea'
+        })
+    });
+    return await response.json();
+};
+
+function RenderResult() {
+    const [apiResponse, setApiResponse] = useState("*** now loading ***");
+
+    useEffect(() => {
+        callRestApi().then(
+            result => setApiResponse(result.photos.map(photo => <ul>
+                    <li>ID: <b>{photo.id}</b></li>
+                    <li>Fotografo: <b>{photo.photographer}</b></li>
+                    <li>URL: {photo.url}</li>
+                    <p><img src={photo.src.small} alt={photo.id}/></p>
+                </ul>
+            )))
+        ;
+    }, []);
+
+    return (
+        <div>
+            <h1>Pexels React Demo</h1>
+            <p>{apiResponse}</p>
+        </div>
+    );
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <RenderResult/>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
